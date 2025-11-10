814eae7cfbf8aa6032dc6e8b59ace32ead87e6ae
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-11-10 11:21:25 +0800

    small update to VA

--- Source/Util/String.cpp
+++ Source/Util/String.cpp
@@ -2,16 +2,29 @@ namespace Util
 {
 	namespace String
 	{
+#define VA_BUFFER_COUNT 4
+#define VA_BUFFER_SIZE 32768
+
 		const char* VA(const char* fmt, ...)
 		{
-			static char buf[32000];
+			static char buffers[VA_BUFFER_COUNT][VA_BUFFER_SIZE];
+			static int currentBuffer = 0;
+
+			char* dest = buffers[currentBuffer];
+			currentBuffer = (currentBuffer + 1) % VA_BUFFER_COUNT;
 
 			va_list args;
 			va_start(args, fmt);
-			vsprintf_s(buf, sizeof(buf), fmt, args);
+			int len = vsnprintf(dest, VA_BUFFER_SIZE, fmt, args);
 			va_end(args);
 
-			return buf;
+			dest[VA_BUFFER_SIZE - 1] = '\0'; // ensure null-termination
+
+			if (len < 0 || len >= VA_BUFFER_SIZE)
+			{
+				printf("Attempted to overrun string in call to va()\n");
+			}
+			return dest;
 		}
 
 		const char* GetCurrentTime()
