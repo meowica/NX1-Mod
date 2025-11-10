bb4e03e2b4013b940ecf38b94361f46078111c76
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-11-10 12:03:38 +0800

    structs tidy up

--- Source/Game/Structs.h
+++ Source/Game/Structs.h
@@ -7,6 +7,7 @@ namespace Structs
 		int nesting;
 		int localClientNum[8];
 		int controllerIndex[8];
+
 		int argc[8];
 		const char** argv[8];
 	};
@@ -14,6 +15,7 @@ namespace Structs
 	struct cmd_function_s
 	{
 		cmd_function_s* next;
+
 		const char* name;
 		const char* autoCompleteDir;
 		const char* autoCompleteExt;
@@ -64,35 +66,43 @@ namespace Structs
 		FloatLimit vector;
 	};
 
-	struct dvar_t
+	typedef struct dvar_s
 	{
 		const char* name;
 		const char* description;
 		unsigned int flags;
 		char type;
 		bool modified;
+
 		DvarValue current;
 		DvarValue latched;
 		DvarValue reset;
 		DvarLimits domain;
-		dvar_t* hashNext;
-	};
+
+		dvar_s* hashNext;
+	} dvar_t;
 
 	struct ScreenPlacement
 	{
 		float scaleVirtualToReal[2];
 		float scaleVirtualToFull[2];
 		float scaleRealToVirtual[2];
+
 		float realViewportPosition[2];
 		float realViewportSize[2];
+
 		float virtualViewableMin[2];
 		float virtualViewableMax[2];
+
 		float realViewableMin[2];
 		float realViewableMax[2];
+
 		float virtualAdjustableMin[2];
 		float virtualAdjustableMax[2];
+
 		float realAdjustableMin[2];
 		float realAdjustableMax[2];
+
 		float subScreenLeft;
 	};
 
@@ -101,6 +111,7 @@ namespace Structs
 		SL_SYSTEM = 0,
 		SL_HUD_BOB_NO_DISTORT,
 		SL_HUD_DISTORT,
+
 		SL_NUM_LAYERS,
 	};
 
@@ -138,67 +149,67 @@ namespace Structs
 		CON_BUILTIN_CHANNEL_COUNT,
 	};
 
-	enum XAssetType : int
+	enum XAssetType
 	{
-		ASSET_TYPE_PHYSPRESET = 0x0,
-		ASSET_TYPE_PHYSCOLLMAP = 0x1,
-		ASSET_TYPE_XANIMPARTS = 0x2,
-		ASSET_TYPE_XMODEL_SURFS = 0x3,
-		ASSET_TYPE_XMODEL = 0x4,
-		ASSET_TYPE_MATERIAL = 0x5,
-		ASSET_TYPE_PIXELSHADER = 0x6,
-		ASSET_TYPE_TECHNIQUE_SET = 0x7,
-		ASSET_TYPE_IMAGE = 0x8,
-		ASSET_TYPE_SOUND = 0x9,
-		ASSET_TYPE_SND_VOLUME_FALLOFF_CURVE = 0xA,
-		ASSET_TYPE_SND_LPF_CURVE = 0xB,
-		ASSET_TYPE_LOADED_SOUND = 0xC,
-		ASSET_TYPE_CLIPMAP_SP = 0xD,
-		ASSET_TYPE_CLIPMAP_MP = 0xE,
-		ASSET_TYPE_COMWORLD = 0xF,
-		ASSET_TYPE_GAMEWORLD_SP = 0x10,
-		ASSET_TYPE_GAMEWORLD_MP = 0x11,
-		ASSET_TYPE_MAP_ENTS = 0x12,
-		ASSET_TYPE_FXWORLD = 0x13,
-		ASSET_TYPE_GFXWORLD = 0x14,
-		ASSET_TYPE_LIGHT_DEF = 0x15,
-		ASSET_TYPE_UI_MAP = 0x16, // never ever used
-		ASSET_TYPE_FONT = 0x17,
-		ASSET_TYPE_MENULIST = 0x18,
-		ASSET_TYPE_MENU = 0x19,
-		ASSET_TYPE_LOCALIZE_ENTRY = 0x1A,
-		ASSET_TYPE_WEAPON = 0x1B,
-		ASSET_TYPE_SNDDRIVER_GLOBALS = 0x1C,
-		ASSET_TYPE_FX = 0x1D,
-		ASSET_TYPE_IMPACT_FX = 0x1E,
-		ASSET_TYPE_SURFACE_FX = 0x1F,
-		ASSET_TYPE_AITYPE = 0x20,
-		ASSET_TYPE_MPTYPE = 0x21,
-		ASSET_TYPE_CHARACTER = 0x22,
-		ASSET_TYPE_XMODELALIAS = 0x23,
-		ASSET_TYPE_RAWFILE = 0x24,
-		ASSET_TYPE_STRINGTABLE = 0x25,
-		ASSET_TYPE_LEADERBOARD = 0x26,
-		ASSET_TYPE_STRUCTURED_DATA_DEF = 0x27,
-		ASSET_TYPE_TRACER = 0x28,
-		ASSET_TYPE_LASER = 0x29,
-		ASSET_TYPE_VEHICLE = 0x2A,
-		ASSET_TYPE_ADDON_MAP_ENTS = 0x2B,
-		ASSET_TYPE_SHOCKFILE = 0x2C,
-		ASSET_TYPE_VOLUMESETTING = 0x2D,
-		ASSET_TYPE_REVERBPRESET = 0x2E,
-		ASSET_TYPE_FOG = 0x2F,
+		ASSET_TYPE_PHYSPRESET,
+		ASSET_TYPE_PHYSCOLLMAP,
+		ASSET_TYPE_XANIMPARTS,
+		ASSET_TYPE_XMODEL_SURFS,
+		ASSET_TYPE_XMODEL,
+		ASSET_TYPE_MATERIAL,
+		ASSET_TYPE_PIXELSHADER,
+		ASSET_TYPE_TECHNIQUE_SET,
+		ASSET_TYPE_IMAGE,
+		ASSET_TYPE_SOUND,
+		ASSET_TYPE_SND_VOLUME_FALLOFF_CURVE,
+		ASSET_TYPE_SND_LPF_CURVE,
+		ASSET_TYPE_LOADED_SOUND,
+		ASSET_TYPE_CLIPMAP_SP,
+		ASSET_TYPE_CLIPMAP_MP,
+		ASSET_TYPE_COMWORLD,
+		ASSET_TYPE_GAMEWORLD_SP,
+		ASSET_TYPE_GAMEWORLD_MP,
+		ASSET_TYPE_MAP_ENTS,
+		ASSET_TYPE_FXWORLD,
+		ASSET_TYPE_GFXWORLD,
+		ASSET_TYPE_LIGHT_DEF,
+		ASSET_TYPE_UI_MAP,
+		ASSET_TYPE_FONT,
+		ASSET_TYPE_MENULIST,
+		ASSET_TYPE_MENU,
+		ASSET_TYPE_LOCALIZE_ENTRY,
+		ASSET_TYPE_WEAPON,
+		ASSET_TYPE_SNDDRIVER_GLOBALS,
+		ASSET_TYPE_FX,
+		ASSET_TYPE_IMPACT_FX,
+		ASSET_TYPE_SURFACE_FX,
+		ASSET_TYPE_AITYPE,
+		ASSET_TYPE_MPTYPE,
+		ASSET_TYPE_CHARACTER,
+		ASSET_TYPE_XMODELALIAS,
+		ASSET_TYPE_RAWFILE,
+		ASSET_TYPE_STRINGTABLE,
+		ASSET_TYPE_LEADERBOARD,
+		ASSET_TYPE_STRUCTURED_DATA_DEF,
+		ASSET_TYPE_TRACER,
+		ASSET_TYPE_LASER,
+		ASSET_TYPE_VEHICLE,
+		ASSET_TYPE_ADDON_MAP_ENTS,
+		ASSET_TYPE_SHOCKFILE,
+		ASSET_TYPE_VOLUMESETTING,
+		ASSET_TYPE_REVERBPRESET,
+		ASSET_TYPE_FOG,
 
-		ASSET_TYPE_COUNT = 0x30,
-		ASSET_TYPE_STRING = 0x30,
-		ASSET_TYPE_ASSETLIST = 0x31,
+		ASSET_TYPE_COUNT,
+		ASSET_TYPE_STRING,
+		ASSET_TYPE_ASSETLIST,
 	};
 
-	struct Font_s; // forced against my will
+	struct Font_s;
 
 	union XAssetHeader
 	{
-		void *data;
+		void* data;
 	}; // incomplete
 
 	struct XAsset
@@ -210,16 +221,21 @@ namespace Structs
 	struct GfxConfiguration
 	{
 		bool inited;
+
 		unsigned int maxClientViews;
 		unsigned int maxClientRenderViews;
+
 		unsigned int entCount;
 		unsigned int entnumNone;
 		unsigned int entnumOrdinaryEnd;
+
 		int threadContextCount;
 		int critSectCount;
+
 		bool defaultFullscreen;
 		unsigned short defaultFullscreenFlags;
 		int defaultMode;
+
 		unsigned int textureMinVRamTier1;
 		unsigned int textureMinVRamTier2;
 	};
