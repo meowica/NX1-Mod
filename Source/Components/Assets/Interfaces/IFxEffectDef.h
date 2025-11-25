aab0acbb8d78a3b0bdbed5fac4e803dec53e6f96
hindercanrun <109132519+meowica@users.noreply.github.com>
2025-11-25 18:36:53 +0800

    Add FX structs

---
+++ Source/Components/Assets/Interfaces/IFxEffectDef.h
@@ -0,0 +1,246 @@
+#pragma once
+
+#include "IMaterial.h"
+
+namespace IFxEffectDef
+{
+	struct FxSpawnDefLooping
+	{
+		int intervalMsec;
+		int count;
+	};
+
+	struct FxIntRange
+	{
+		int base;
+		int amplitude;
+	};
+
+	struct FxSpawnDefOneShot
+	{
+		FxIntRange count;
+	};
+
+	union FxSpawnDef
+	{
+		FxSpawnDefLooping looping;
+		FxSpawnDefOneShot oneShot;
+	};
+
+	struct FxElemAtlas
+	{
+		float fps;
+		short entryCount;
+		unsigned char behavior;
+		unsigned char index;
+		unsigned char loopCount;
+		unsigned char colIndexBits;
+		unsigned char rowIndexBits;
+	};
+
+	struct FxElemVec3Range
+	{
+		float base[3];
+		float amplitude[3];
+	};
+
+	struct FxElemVelStateInFrame
+	{
+		FxElemVec3Range velocity;
+		FxElemVec3Range totalDelta;
+	};
+
+	struct FxElemVelStateSample
+	{
+		FxElemVelStateInFrame local;
+		FxElemVelStateInFrame world;
+	};
+
+	struct FxElemVisualState
+	{
+		unsigned char color[4];
+		float rotationDelta;
+		float rotationTotal;
+		float size[2];
+		float scale;
+	};
+
+	const struct FxElemVisStateSample
+	{
+		FxElemVisualState base;
+		FxElemVisualState amplitude;
+	};
+
+	struct FxElemMarkVisuals
+	{
+		IMaterial::Material* materials[2];
+	};
+
+	struct FxEffectDef;
+
+	union FxEffectDefRef
+	{
+		FxEffectDef* handle;
+		const char *name;
+	};
+
+	union FxElemVisuals
+	{
+		const void* anonymous;
+		IMaterial::Material* material;
+		/*XModel* model;*/
+		FxEffectDefRef effectDef;
+		const char* soundName;
+	};
+
+	union FxElemDefVisuals
+	{
+		FxElemMarkVisuals* markArray;
+		FxElemVisuals* array;
+		FxElemVisuals instance;
+	};
+
+	struct Bounds
+	{
+		float midPoint[3];
+		float halfSize[3];
+	};
+
+	struct FxFloatRange
+	{
+		float base;
+		float amplitude;
+	};
+
+	struct FxTrailVertex
+	{
+		float pos[2];
+		float normal[2];
+		float texCoord;
+	};
+
+	struct FxTrailDef
+	{
+		int scrollTimeMsec;
+		int repeatDist;
+		float invSplitDist;
+		float invSplitArcDist;
+		float invSplitTime;
+		int vertCount;
+		FxTrailVertex* verts;
+		int indCount;
+		unsigned short* inds;
+	};
+
+	struct FxSparkFountainDef
+	{
+		float gravity;
+		float bounceFrac;
+		float bounceRand;
+		float sparkSpacing;
+		float sparkLength;
+		int sparkCount;
+		float loopTime;
+		float velMin;
+		float velMax;
+		float velConeFrac;
+		float restSpeed;
+		float boostTime;
+		float boostFactor;
+	};
+
+	union FxElemExtendedDefPtr
+	{
+		FxTrailDef* trailDef;
+		FxSparkFountainDef* sparkFountainDef;
+		void* unknownDef;
+	};
+
+	struct FxElemDef
+	{
+		int flags;
+		FxSpawnDef spawn;
+		FxFloatRange spawnRange;
+		FxFloatRange fadeInRange;
+		FxFloatRange fadeOutRange;
+		float spawnFrustumCullRadius;
+		FxIntRange spawnDelayMsec;
+		FxIntRange lifeSpanMsec;
+		FxFloatRange spawnOrigin[3];
+		unsigned char m_beam_color[5][4];
+		FxFloatRange spawnOffsetRadius;
+		FxFloatRange spawnOffsetHeight;
+		FxFloatRange spawnAngles[3];
+		FxFloatRange angularVelocity[3];
+		FxFloatRange initialRotation;
+		FxFloatRange gravity;
+		FxFloatRange reflectionFactor;
+		FxElemAtlas atlas;
+		int runnerPreSeed;
+		unsigned char elemType;
+		unsigned char visualCount;
+		unsigned char velIntervalCount;
+		unsigned char visStateIntervalCount;
+		FxElemVelStateSample* velSamples;
+		FxElemVisStateSample* visSamples;
+		FxElemDefVisuals visuals;
+		Bounds collBounds;
+		FxEffectDefRef effectOnImpact;
+		FxEffectDefRef effectOnDeath;
+		FxEffectDefRef effectEmitted;
+		FxFloatRange emitDist;
+		FxFloatRange emitDistVariance;
+		FxElemExtendedDefPtr extended;
+		unsigned char sortOrder;
+		unsigned char lightingFrac;
+		unsigned char useItemClip;
+		unsigned char fadeInfo;
+		int randomSeed;
+		float m_facing[3];
+		int m_laser_shape;
+		float m_laser_width;
+		float velIntervalCountF;
+		float m_laser_segment;
+		float m_laser_texture_animation;
+		int m_tracer_draw_interval;
+		float m_tracer_length;
+		float m_tracer_width;
+		float m_tracer_speed;
+		float m_tracer_screw_distance;
+		float m_tracer_screw_radius;
+		int m_tracer_screw_radius_expansion_start_a;
+		int m_tracer_screw_radius_expansion_start_b;
+		float m_tracer_screw_radius_expansion_overtime_a;
+		float m_tracer_screw_radius_expansion_overtime_b;
+		int m_tracer_fadeout_begin_a;
+		int m_tracer_fadeout_begin_b;
+		int m_tracer_fadeout_duration_a;
+		int m_tracer_fadeout_duration_b;
+	};
+
+	struct FxEffectDef
+	{
+		const char* name;
+		int flags;
+		int totalSize;
+		int msecLoopingLife;
+		int elemDefCountLooping;
+		int elemDefCountOneShot;
+		int elemDefCountEmission;
+		FxElemDef* elemDefs;
+	};
+
+#ifdef IS_MP
+	namespace MP
+	{
+		void Load();
+		void Unload();
+	}
+#elif IS_SP
+	namespace SP
+	{
+		void Load();
+		void Unload();
+	}
+#endif
+}
